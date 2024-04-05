f785dedf2c53805e3dc8bcfd92722417524e2c0c
Ivan Hlukhenkyi <105237714+m0rg1nal@users.noreply.github.com>
2024-04-05 21:42:26 +0300

    Update task1.py

--- Loops + sets/task1.py
+++ Loops + sets/task1.py
@@ -3,4 +3,4 @@ second = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 first_set = set(first)
 second_set = set(second)
 united = first_set.intersection(second_set)
-print(united)
+print(f'{united} - list of intersecting numbers in two lists.')
